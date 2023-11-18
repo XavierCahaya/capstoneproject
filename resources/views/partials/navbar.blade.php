98c5e57b84d80b01c2344d0627b0e48b5e920ea3
Shrmanto <shrmantoch@gmail.com>
2023-11-18 11:08:38 +0700

    feat: menambah fitur cek pesanan

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -5,7 +5,7 @@
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mx-auto mb-2 mb-lg-0"> 
+            <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
                 <li class="nav-item">
                     <a class="nav-link {{ request()->is('/') ? 'active' : '' }}" href="/">Beranda</a>
                 </li>
@@ -16,7 +16,7 @@
                     <a class="nav-link {{ request()->is('kategori') ? 'active' : '' }}" href="/kategori">Kategori</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link {{ request()->is('cekPesanan') ? 'active' : '' }}" href="/cekPesanan">Cek Pesanan</a>
+                    <a class="nav-link {{ request()->is('cekpesanan') ? 'active' : '' }}" href="{{ route('cekpesanan') }}">Cek Pesanan</a>
                 </li>
             </ul>
             <ul class="navbar-nav">
