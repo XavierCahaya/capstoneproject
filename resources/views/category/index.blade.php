17c4cc0b40978430aba4fd67ba36545ca9df2378
Rena Egina <100079277+renaegina@users.noreply.github.com>
2023-11-21 22:35:59 +0700

    Update index.blade.php

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -1,23 +1,11 @@
 @extends('layouts.main')
 
 @section('container')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-lg-6">
-            <form action="#" method="GET">
-                <div class="input-group mb-4">
-                    <input type="text" class="form-control" placeholder="Search..." name="search" value="{{ request('search') }}">
-                    <button class="btn" type="submit" style="background: #FC3E24; color:white">search</button>
-                </div>
-            </form>
-        </div>
-    </div>
-</div>
 <div class="row">
     <div class="col-lg-12 mb-4">
-        <div class="hstack gap-3">
+        <div class="hstack gap-3 d-flex justify-content-center">
             @foreach ($categories as $category)
-            <div class="bg-light border">
+            <div class="bg-light border my-4" style="border-radius: 20px;">
                 <div class="card-product card" style="width: 15rem; border-radius:20px;">
                     <img src="/image/food.webp" class="card-img-top img-fluid" style="border-radius:20px 20px 0 0;" alt="...">
                     <div class="card-body">
