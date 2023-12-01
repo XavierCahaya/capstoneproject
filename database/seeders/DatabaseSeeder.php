8c9ba4aba62d1f704236e4b7caaa34ffd1239759
shrmanto <shrmantoch@gmail.com>
2023-12-01 21:22:24 +0700

    fix: seeder product menu

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -64,5 +64,129 @@ class DatabaseSeeder extends Seeder
                 echo "File gambar tidak ditemukan: {$promo['image']}\n";
             }
         }
+
+        $productData = [
+            [
+                'category_id' => '2',
+                'name' => 'Mie Jebew Ori',
+                'description' => 'Mie Jebew Original Taste',
+                'price' => '8000',
+                'image' => 'mie_jebew.webp',
+                'status' =>  'aktif',
+            ],
+            [
+                'category_id' => '2',
+                'name' => 'Mie Jebew Komplit',
+                'description' => 'Mie Jebew Komplit Taste',
+                'price' => '10000',
+                'image' => 'mie_jebew_komplit.webp',
+                'status' =>  'aktif',
+            ],
+            [
+                'category_id' => '2',
+                'name' => 'I-Katszu',
+                'description' => 'I-Katzsu taste',
+                'price' => '8000',
+                'image' => 'i-katszu.png',
+                'status' =>  'aktif',
+            ],
+            [
+                'category_id' => '3',
+                'name' => 'Spicy Wonton',
+                'description' => 'Spicy Wonton Taste',
+                'price' => '8000',
+                'image' => 'spicy_wonton.webp',
+                'status' =>  'aktif',
+            ],
+            [
+                'category_id' => '3',
+                'name' => 'Wonton Crishpy',
+                'description' => 'Wonton Crishpy Taste',
+                'price' => '8000',
+                'image' => 'wonton_crishpy.jpg',
+                'status' =>  'aktif',
+            ],
+            [
+                'category_id' => '1',
+                'name' => 'Mojito Mangga',
+                'description' => 'Mojito Taste',
+                'price' => '5000',
+                'image' => 'mojito_mangga.webp',
+                'status' =>  'aktif',
+            ],
+            [
+                'category_id' => '1',
+                'name' => 'Mojito Melon',
+                'description' => 'Mojito Taste',
+                'price' => '5000',
+                'image' => 'mojito_melon.webp',
+                'status' =>  'aktif',
+            ],
+            [
+                'category_id' => '1',
+                'name' => 'Mojito Jeruk',
+                'description' => 'Mojito Taste',
+                'price' => '5000',
+                'image' => 'mojito_jeruk.webp',
+                'status' =>  'aktif',
+            ],
+            [
+                'category_id' => '1',
+                'name' => 'Mojito Leci',
+                'description' => 'Mojito Taste',
+                'price' => '5000',
+                'image' => 'mojito_leci.webp',
+                'status' =>  'aktif',
+            ],
+            [
+                'category_id' => '1',
+                'name' => 'Mojito Stawberry',
+                'description' => 'Mojito Taste',
+                'price' => '5000',
+                'image' => 'mojito_strawberry.webp',
+                'status' =>  'aktif',
+            ],
+            [
+                'category_id' => '1',
+                'name' => 'Es Lemon Tea',
+                'description' => 'Es Lemon Tea Taste',
+                'price' => '3000',
+                'image' => 'es-lemon-tea.jpg',
+                'status' =>  'aktif',
+            ],
+            [
+                'category_id' => '1',
+                'name' => 'Es Jeruk',
+                'description' => 'Es Jeruk Taste',
+                'price' => '3000',
+                'image' => 'es-jeruk.jpg',
+                'status' =>  'aktif',
+            ],
+            [
+                'category_id' => '1',
+                'name' => 'Air Mineral Gelas',
+                'description' => 'Air Mineral Gelas Taste',
+                'price' => '1000',
+                'image' => 'air_mineral.jpg',
+                'status' =>  'aktif',
+            ],
+        ];
+    
+        foreach ($productData as $product) {
+            $imagePath = public_path('images/product/' . $product['image']);
+    
+            if (File::exists($imagePath)) {
+                Product::create([
+                    'category_id' => $product['category_id'],
+                    'name' => $product['name'],
+                    'description' => $product['description'],
+                    'price' => $product['price'],
+                    'image' => $product['image'],
+                    'status' =>  $product['status'],
+                ]);
+            } else {
+                echo "File gambar tidak ditemukan: {$product['image']}\n";
+            }
+        }
     }
 }
