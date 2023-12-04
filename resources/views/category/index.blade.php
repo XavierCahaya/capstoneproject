6df0eaded1a3f11debf808cac91cc696a75fe715
renaegina <renaegina25778@gmail.com>
2023-12-04 16:14:15 +0700

    Update Profile

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -5,13 +5,13 @@
     <div class="col-lg-12 mb-4">
         <div class="hstack gap-3 justify-content-center">
             @foreach ($categories as $category)
-            <div class="bg-light border my-4" style="border-radius: 20px;">
-                <div class="card-product card" style="width: 12rem; border-radius:20px;">
-                    <img src="/image/food.webp" class="card-img-top img-fluid" style="border-radius:20px 20px 0 0;" alt="...">
+            <div class="bg-light border my-4" style="border-radius: 20px; border: 2px solid #000000; padding: 3px;">
+                <div class="card-product card" style="width: 12rem; border-radius:20px;">                   
+                    <img src="/image/categories/{{ strtolower($category->name) }}.webp" class="card-img-top img-fluid" style="border-radius:20px 20px 0 0; object-fit: cover;" alt="...">
                     <div class="card-body">
                         <p class="card-text text-center" style="font-size: 15px">{{ $category->name }}</p>
                         <div class="d-flex justify-content-around">
-                            <a href="{{route('category.show', $category->id)}}" class="btn btn-detail" style="background-color:#1FD8CD; border-radius:10px; font-size:12px; padding:8px;">
+                            <a href="{{route('category.show', $category->id)}}" class="btn btn-detail" style="background-color:#1f7fd8; color: #ffffff; border-radius:10px; font-size:13px; padding:10px;">
                             Lihat Detail
                         </a>
                         </div>
